c9ec1b8821765ac1878327f166f63b7c92511807
Alexander Samilovskikh <alex.kmoxville@gmail.com>
2024-03-14 23:32:39 +0300

    Deleted obsolete files

--- src/config.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Config:
-    def __init__(self, token: str):
-        self.token = token
-    token: str
-    api_url: str = 'https://api.telegram.org/bot'
